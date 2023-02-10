2a56d55b887915108a244c7d9227e5f8c8f4543a
HuseyinKutlukkaya <huseyinkutlukkaya@gmail.com>
2023-02-10 21:59:11 +0300

    Added Crud for products

---
+++ PatikaRestfulApiOdev/Controllers/ProductController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PatikaRestfulApiOdev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly List<Product> _products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Product 1", Price = 10, Description = "product 1" },
+        new Product { Id = 2, Name = "Product 2", Price = 10, Description = "product 2" },
+        new Product { Id = 3, Name = "Product 3", Price = 10, Description = "product 3" }
+    };
+
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById([FromBody] int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Post([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Product> Put(int id, [FromBody] Product product)
+        {
+            var existingProduct = _products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+            existingProduct.Description = product.Description;
+            return Ok(existingProduct);
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<Product> Delete(int id)
+        {
+            var existingProduct = _products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            _products.Remove(existingProduct);
+            return Ok(existingProduct);
+        }
+
+        [HttpPatch("{id}")]
+        public ActionResult<Product> Patch(int id, [FromBody] JsonPatchDocument<Product> patch)
+        {
+            var existingProduct = _products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            List<JsonPatchError> errors = new List<JsonPatchError>();
+            patch.ApplyTo(existingProduct, e => errors.Add(e));
+
+            if (errors.Count > 0)
+            {
+                return BadRequest(errors);
+            }
+
+            return Ok(existingProduct);
+        }
+    }
+}
---
+++ PatikaRestfulApiOdev/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatikaRestfulApiOdev
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
